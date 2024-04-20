2f7377080349f726944e779d543ce545b123bb14
Nexs12 <152890031+Nexs12@users.noreply.github.com>
2024-04-20 20:30:17 +0300

    HWJS

--- js/task-2.js
+++ js/task-2.js
@@ -1,1 +1,34 @@
 'use strict';
+
+class Storage {
+    #items;
+
+    constructor(initialItems = []) {
+    this.#items = initialItems;
+  }
+    getItems() {
+    return this.#items;
+  }
+    addItem(newItem) {
+    this.#items.push(newItem);
+  }
+    removeItem(itemToRemove) {
+    const item = this.#items.indexOf(itemToRemove);
+    if (item !== -1) {
+      this.#items.splice(item, 1);
+    }
+  }
+}
+
+
+const storage = new Storage(["Nanitoids", "Prolonger", "Antigravitator"]);
+console.log(storage.getItems()); // ["Nanitoids", "Prolonger", "Antigravitator"]
+
+storage.addItem("Droid");
+console.log(storage.getItems()); // ["Nanitoids", "Prolonger", "Antigravitator", "Droid"]
+
+storage.removeItem("Prolonger");
+console.log(storage.getItems()); // ["Nanitoids", "Antigravitator", "Droid"]
+
+storage.removeItem("Scaner");
+console.log(storage.getItems()); // ["Nanitoids", "Antigravitator", "Droid"]
